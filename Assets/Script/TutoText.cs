dda5ebc0c5cf7bdfe9d86477f0ffbe09292f33bf
kangtp <115209262+kangtp@users.noreply.github.com>
2023-11-30 19:21:26 +0900

    +tutorial

---
+++ Assets/Script/TutoText.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TutoText : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(appearText());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    IEnumerator appearText()
+    {
+        yield return new WaitForSeconds(2f);
+        transform.GetChild(0).gameObject.SetActive(true);
+    }
+}
