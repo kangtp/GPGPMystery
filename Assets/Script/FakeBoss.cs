30daef6fa1bb9f4da13aed12c7c24872763269e7
kangtp <115209262+kangtp@users.noreply.github.com>
2023-12-23 01:34:24 +0900

    .

--- Assets/Script/FakeBoss.cs
+++ Assets/Script/FakeBoss.cs
@@ -32,6 +32,7 @@ public class FakeBoss : MonoBehaviour
     IEnumerator Done()
     {
         ShakeScreen.Instance.Callshake(); // 화면 흔들림 함수 호출
+        GameObject.Find("GameCanvas").transform.GetChild(10).gameObject.SetActive(true);
         yield return new WaitForSeconds(2f);
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
     }
