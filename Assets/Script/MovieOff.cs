abc00a791e8901b92028f28e92c84421d150c734
kangtp <115209262+kangtp@users.noreply.github.com>
2023-12-23 03:32:23 +0900

    final

--- Assets/Script/MovieOff.cs
+++ Assets/Script/MovieOff.cs
@@ -12,8 +12,6 @@ public class MovieOff : MonoBehaviour
     void Awake()
     {
         StartCoroutine(screenOff());
-        PlayerPrefs.DeleteKey("Stage");
-        PlayerPrefs.DeleteKey("Exist");
     }
 
 
